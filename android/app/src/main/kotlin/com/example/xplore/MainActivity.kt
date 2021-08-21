0c1567d733e4418c8739d51c5b48626ea06b1203
abdi-adan <adanabdi036@gmail.com>
2021-08-21 17:31:33 +0300

    xplore: initial commit

---
+++ android/app/src/main/kotlin/com/example/xplore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xplore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/core/xplore.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class XploreApp extends StatelessWidget {
+  const XploreApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
