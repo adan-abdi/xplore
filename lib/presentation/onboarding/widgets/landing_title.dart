63137beb7720ef276870336df04d9ec37d84260d
abdi-adan <adanabdi036@gmail.com>
2022-05-01 00:57:59 +0300

    fix: landing page dynamic flex layout resolve #52

--- lib/presentation/onboarding/widgets/landing_title.dart
+++ lib/presentation/onboarding/widgets/landing_title.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 // Project imports:
 import 'package:xplore/application/core/themes/colors.dart';
 import 'package:xplore/domain/value_objects/app_magic_numbers.dart';
-import 'package:xplore/domain/value_objects/app_spaces.dart';
 
 class LandingPageTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final hSizeDimension = MediaQuery.of(context).size.height;
+    final mainTitleFontSize = hSizeDimension * 0.07;
+    final andFontSize = hSizeDimension * 0.05;
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 20),
+      padding: EdgeInsets.symmetric(vertical: 5),
       child: Column(
         children: [
           RichText(
@@ -19,30 +21,29 @@ class LandingPageTitle extends StatelessWidget {
               text: 'Fast \n',
               style: TextStyle(
                   color: XploreColors.deepBlue,
-                  fontSize: 55,
+                  fontSize: mainTitleFontSize,
                   fontWeight: FontWeight.bold),
-              children: const <TextSpan>[
+              children: <TextSpan>[
                 TextSpan(
                     text: '   & ',
                     style: TextStyle(
                       color: XploreColors.black,
-                      fontSize: 48,
+                      fontSize: andFontSize,
                       fontWeight: FontWeight.normal,
                     )),
                 TextSpan(
                   text: 'Digital.',
                   style: TextStyle(
                     color: XploreColors.deepBlue,
-                    fontSize: 55,
+                    fontSize: mainTitleFontSize,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ],
             ),
           ),
-          vSize30SizedBox,
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
             child: Text(
               'Keep your store in order , Easy to use  point of sale',
               style: TextStyle(
