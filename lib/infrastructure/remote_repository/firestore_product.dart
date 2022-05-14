ec0455730cfc3545590b415b97307b3fefdbad8b
abdi-adan <adanabdi036@gmail.com>
2022-05-15 01:03:28 +0300

    add firestore repository classes

---
+++ lib/infrastructure/remote_repository/firestore_product.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shamiri/domain/models/product.dart';
+
+class ProductRepository {
+  final CollectionReference collection =
+      FirebaseFirestore.instance.collection('products');
+  
+  Stream<QuerySnapshot> getStream() {
+    return collection.snapshots();
+  }
+  
+  Future<DocumentReference> addProduct(Product product) {
+    return collection.add(product.toJson());
+  }
+  
+  void updateProduct(Product product) async {
+    await collection.doc(product.referenceId).update(product.toJson());
+  }
+  
+  void deleteProduct(Product product) async {
+    await collection.doc(product.referenceId).delete();
+  }
+}
---
+++ lib/infrastructure/remote_repository/firestore_user.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shamiri/domain/models/product.dart';
+import 'package:shamiri/domain/models/user.dart';
+
+class UserRepository {
+  final CollectionReference collection =
+      FirebaseFirestore.instance.collection('users');
+  
+  Stream<QuerySnapshot> getStream() {
+    return collection.snapshots();
+  }
+  
+  Future<DocumentReference> addUser(User user) {
+    return collection.add(user.toJson());
+  }
+  
+  void updateUser(User user) async {
+    await collection.doc(user.referenceId).update(user.toJson());
+  }
+  
+  void deletePet(User user) async {
+    await collection.doc(user.referenceId).delete();
+  }
+}
