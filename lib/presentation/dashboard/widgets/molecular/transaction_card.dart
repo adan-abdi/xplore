60bf3ef40fbafb911b525b4d5704c833a6b4d466
abdi-adan <adanabdi036@gmail.com>
2022-06-01 21:55:43 +0300

    redesign checkout item card

--- lib/presentation/dashboard/widgets/molecular/transaction_card.dart
+++ lib/presentation/dashboard/widgets/molecular/transaction_card.dart
@@ -1,37 +1,111 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shamiri/application/core/themes/colors.dart';
+import 'package:shamiri/domain/value_objects/app_spaces.dart';
 
 class Transactioncard extends StatelessWidget {
   final String name;
   final String quantity;
+  final String date;
+  final String price;
 
   Transactioncard({
     Key? key,
     required this.name,
     required this.quantity,
+    required this.date,
+    required this.price,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var amount = price.toUpperCase();
+    var dateParsed = DateFormat('yyyy-MM-dd HH:mm').parse(date);
+    var dateOrdered = DateFormat.yMMMd().format(dateParsed);
     return Container(
-        padding: EdgeInsets.all(0),
-        height: 90,
-        child: ListTile(
-          leading: Icon(
-            Icons.receipt_long,
+      padding: EdgeInsets.all(0),
+      height: 100,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            child: Container(
+              color: XploreColors.xploreOrange.withOpacity(.3),
+              width: 40,
+              height: 40,
+              child: Icon(
+                Icons.receipt_long,
+                color: XploreColors.xploreOrange,
+              ),
+            ),
           ),
-          title: Text("  $name",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 20,
-              )),
-          subtitle: Text("  $quantity Items",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.grey,
-              )),
-        ));
+          hSize20SizedBox,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$name",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  "$dateOrdered",
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: XploreColors.deepBlue,
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  "KES $amount",
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          hSize20SizedBox,
+          Column(
+            children: [
+              MaterialButton(
+                  color: XploreColors.deepBlue,
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      InkWell(
+                        child: Icon(
+                          Icons.add,
+                          color: XploreColors.white,
+                        ),
+                      ),
+                      hSize10SizedBox,
+                      Text(
+                        '$quantity',
+                        style: TextStyle(color: XploreColors.white),
+                      ),
+                      hSize10SizedBox,
+                      InkWell(
+                        child: Icon(
+                          Icons.remove,
+                          color: XploreColors.white,
+                        ),
+                      ),
+                    ],
+                  )),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
