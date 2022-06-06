2729c17477206d57df10a4da6264d51193d3e038
abdi-adan <adanabdi036@gmail.com>
2022-06-06 22:16:07 +0300

    swap bottomnavbar

--- lib/presentation/core/pages/dashboard_scaffold.dart
+++ lib/presentation/core/pages/dashboard_scaffold.dart
@@ -1,25 +1,21 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-
-// Package imports:
-import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:shamiri/application/core/themes/colors.dart';
 
 // Project imports:
-import 'package:shamiri/application/core/themes/colors.dart';
 import 'package:shamiri/application/singletons/dashboard_current_index.dart';
 import 'package:shamiri/application/singletons/pending_items_store.dart';
 import 'package:shamiri/application/singletons/product_listing_status.dart';
 import 'package:shamiri/application/singletons/search_state.dart';
 import 'package:shamiri/domain/routes/routes.dart';
 import 'package:shamiri/domain/value_objects/app_strings.dart';
-import 'package:shamiri/infrastructure/remote_repository/inventory/firebase_inventory.dart';
 import 'package:shamiri/infrastructure/remote_repository/inventory/firestore_product.dart';
 import 'package:shamiri/infrastructure/remote_repository/inventory/firestore_transaction.dart';
 import 'package:shamiri/presentation/core/widgets/layout/xplore_appbar.dart';
 import 'package:shamiri/presentation/core/widgets/molecular/dashboard_tab_action_button.dart';
 import 'package:shamiri/presentation/core/widgets/xplore_card.dart';
 import 'package:shamiri/presentation/core/widgets/xplore_snackbar.dart';
-import 'package:shamiri/presentation/dashboard/widgets/layout/dashboard_shimmer.dart';
 
 // ignore: must_be_immutable
 class DashboardScaffold extends StatefulWidget {
@@ -57,10 +53,8 @@ class _DashboardScaffoldState extends State<DashboardScaffold> {
 
   @override
   Widget build(BuildContext context) {
-    InventoryRepository inventoryRepository = InventoryRepository();
     int activeTab = widget.dashboardIndexStatusStore.currentIndex.value;
-    String appBarTitle =
-        (activeTab == 0) ? 'Merchant Store' : 'Merchant Records';
+    String appBarTitle = (activeTab == 0) ? 'Merchant Store' : 'Merchant Records';
 
     return WillPopScope(
       onWillPop: () async => false,
@@ -89,59 +83,48 @@ class _DashboardScaffoldState extends State<DashboardScaffold> {
               ? XploreIconCard(
                   icon: Icons.unfold_less,
                   iconOnPress: () {
-                    // setState(() {
-                    //   // isAppbarExpanded = !isAppbarExpanded;
-                    // });
+                    ScaffoldMessenger.of(context).showSnackBar(snackbar(
+                      content: comingSoonText,
+                    ));
                   },
                 )
               : XploreIconCard(
                   icon: Icons.unfold_more,
                   iconOnPress: () {
-                    // setState(() {
-                    //   isAppbarExpanded = !isAppbarExpanded;
-                    // });
+                    ScaffoldMessenger.of(context).showSnackBar(snackbar(
+                      content: comingSoonText,
+                    ));
                   },
                 ),
           expanded: isAppbarExpanded,
         ),
-        body: StreamBuilder(
-            stream: inventoryRepository.getStream(),
-            builder: (context, snapshot) {
-              if (snapshot.data != null) {
-                return widget
-                    .tabs[widget.dashboardIndexStatusStore.currentIndex.value];
-              }
-              return DashboardShimmer();
-            }),
+        body: widget.tabs[widget.dashboardIndexStatusStore.currentIndex.value],
         floatingActionButton: DashboardTabActionFAB(
           actionIcon: activeTab == 0 ? Icons.add_circle : Icons.receipt,
           actionLabel: activeTab == 0 ? addProducts : fulfillAll,
           onPressed: () async {
             activeTab == 0
-                ? await Navigator.pushReplacementNamed(
-                    context, addProductPageRoute,
+                ? await Navigator.pushReplacementNamed(context, addProductPageRoute,
                     arguments: widget.productRepoInstance)
-                : fulfillAllOrders(
-                    widget.pendingOrdersStore.pendingItems.value);
+                : fulfillAllOrders(widget.pendingOrdersStore.pendingItems.value);
           },
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        bottomNavigationBar: FlashyTabBar(
-          selectedIndex: widget.dashboardIndexStatusStore.currentIndex.value,
-          showElevation: true,
-          onItemSelected: (index) => setState(() {
-            widget.dashboardIndexStatusStore.currentIndex.value = index;
-          }),
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: widget.dashboardIndexStatusStore.currentIndex.value,
+          onTap: (i) => setState(() => widget.dashboardIndexStatusStore.currentIndex.add(i)),
           items: [
-            FlashyTabBarItem(
+            SalomonBottomBarItem(
               icon: Icon(Icons.storefront),
-              title: Text('Shop'),
-              activeColor: XploreColors.xploreOrange,
+              title: Text("Store"),
+              selectedColor: XploreColors.xploreOrange,
+              unselectedColor: XploreColors.deepBlue,
             ),
-            FlashyTabBarItem(
+            SalomonBottomBarItem(
               icon: Icon(Icons.shopping_cart_checkout),
-              title: Text('Transactions'),
-              activeColor: XploreColors.xploreOrange,
+              title: Text("Transactions"),
+              selectedColor: XploreColors.xploreOrange,
+              unselectedColor: XploreColors.deepBlue,
             ),
           ],
         ),
@@ -150,11 +133,8 @@ class _DashboardScaffoldState extends State<DashboardScaffold> {
   }
 
   Future<void> fulfillAllOrders(List<String> pendingItemsRefIdList) async {
-    for (int i = 0;
-        i < int.parse(pendingItemsRefIdList.length.toString());
-        i++) {
-      await widget.transactionRepository
-          .fulfillTransaction(pendingItemsRefIdList[i]);
+    for (int i = 0; i < int.parse(pendingItemsRefIdList.length.toString()); i++) {
+      await widget.transactionRepository.fulfillTransaction(pendingItemsRefIdList[i]);
     }
   }
 }
