5ee3406af00c999ea3effca2b71df28318946cae
KenStarry <starrycodes@gmail.com>
2023-07-29 00:18:06 +0300

    Added Sign in with phone functionality to auth controller

--- lib/core/presentation/controller/auth_controller.dart
+++ lib/core/presentation/controller/auth_controller.dart
@@ -3,6 +3,12 @@ import 'package:shamiri/core/domain/use_cases/auth_use_cases.dart';
 import 'package:shamiri/di/locator.dart';
 
 class AuthController extends GetxController {
-
   final authUseCases = locator.get<AuthUseCases>();
+
+  /// sign in with phone
+  Future<void> signInWithPhone(
+          {required String phoneNumber,
+          required Function(String verificationId) onCodeSent}) async =>
+      await authUseCases.signInWithPhone
+          .call(phoneNumber: phoneNumber, onCodeSent: onCodeSent);
 }
