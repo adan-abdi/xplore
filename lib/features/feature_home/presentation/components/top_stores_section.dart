3b8fcad18183c0432b1bf279abdbb23216997077
KenStarry <starrycodes@gmail.com>
2023-07-29 13:35:14 +0300

    Carousel Slider

--- lib/features/feature_home/presentation/components/top_stores_section.dart
+++ lib/features/feature_home/presentation/components/top_stores_section.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:shamiri/application/core/themes/colors.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class TopStoresSection extends StatefulWidget {
   const TopStoresSection({super.key});
@@ -8,8 +11,58 @@ class TopStoresSection extends StatefulWidget {
 }
 
 class _TopStoresSectionState extends State<TopStoresSection> {
+
+  late final CarouselController _carouselController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _carouselController = CarouselController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          //  top stores carousel
+          CarouselSlider.builder(
+              itemCount: 3,
+              itemBuilder: (context, index, realIndex) => Container(color: Colors.red,),
+              carouselController: _carouselController,
+              options: CarouselOptions(
+                  height: 200,
+                  initialPage: 0,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: true,
+                  viewportFraction: 0.9,
+                  scrollPhysics:
+                  const BouncingScrollPhysics(),
+                  onPageChanged: (index, reason) {
+
+                  })),
+
+          const SizedBox(
+            height: 8,
+          ),
+
+          //  carousel indicators
+          AnimatedSmoothIndicator(
+            activeIndex: 0,
+            count: 3,
+            effect: ExpandingDotsEffect(
+                dotHeight: 8,
+                dotWidth: 8,
+                activeDotColor: XploreColors.xploreOrange,
+                dotColor: XploreColors.deepBlue.withOpacity(0.2)),
+            onDotClicked: (index) {
+              _carouselController.animateToPage(index);
+            },
+          ),
+          //  top stores carousel indicators
+        ],
+      ),
+    );
   }
 }
