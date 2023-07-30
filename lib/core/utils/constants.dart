111e053435a9df859a37b5ab7ff424c339d68124
KenStarry <starrycodes@gmail.com>
2023-07-30 16:39:37 +0300

    Add Product to Firebase Implementation

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,6 +2,7 @@ class Constants {
 
   /// Firebase
   static const USER_COLLECTION = 'users';
+  static const PRODUCTS_COLLECTION = 'products';
 
   /// Hive
   static const USER_PREFS_BOX = 'user_prefs';
--- lib/features/feature_merchant_store/data/repository/merchant_repository_impl.dart
+++ lib/features/feature_merchant_store/data/repository/merchant_repository_impl.dart
@@ -1,11 +1,22 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:hive/hive.dart';
 import 'package:shamiri/features/feature_merchant_store/domain/repository/merchant_repository.dart';
 
 import '../../../../core/domain/model/response_state.dart';
+import '../../../../core/utils/constants.dart';
+import '../../../../di/locator.dart';
 import '../../domain/model/product_model.dart';
 
 class MerchantRepositoryImpl implements MerchantRepository {
+  final userPrefsBox = Hive.box(Constants.USER_PREFS_BOX);
+  final auth = locator.get<FirebaseAuth>();
+  final firestore = locator.get<FirebaseFirestore>();
+  final storage = locator.get<FirebaseStorage>();
+
   /// Add Product to Firestore
   @override
   Future<void> addProductToFirestore(
@@ -13,7 +24,39 @@ class MerchantRepositoryImpl implements MerchantRepository {
       required File? productPic,
       required Function(ResponseState response) response,
       required Function onSuccess}) async {
+    response(ResponseState.loading);
 
+    try {
+      if (productPic != null) {
+        await storeFileToFirebaseStorage(
+                ref: 'productPics/${auth.currentUser!.uid}', file: productPic)
+            .then((downloadUrl) => product.productImageUrl = downloadUrl);
+      }
 
+      final productId = "${product.productName!}-${product.productCreatedAt!}";
+
+      await firestore
+          .collection(Constants.USER_COLLECTION)
+          .doc(auth.currentUser!.uid)
+          .collection(Constants.PRODUCTS_COLLECTION)
+          .doc(productId)
+          .set(product.toMap())
+          .then((value) {
+        response(ResponseState.success);
+        onSuccess();
+      });
+    } on FirebaseException catch (error) {
+      response(ResponseState.failure);
+      throw Exception(error);
+    }
+  }
+
+  Future<String> storeFileToFirebaseStorage(
+      {required String ref, required File file}) async {
+    final UploadTask uploadTask = storage.ref().child(ref).putFile(file);
+    final TaskSnapshot taskSnapshot = await uploadTask;
+
+    final String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+    return downloadUrl;
   }
 }
