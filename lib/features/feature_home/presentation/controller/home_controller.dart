fe36eb485dce464fea8873756cc7ab4c60c1c40c
KenStarry <starrycodes@gmail.com>
2023-08-01 11:57:35 +0300

    Get All Products Stream in home controller

--- lib/features/feature_home/presentation/controller/home_controller.dart
+++ lib/features/feature_home/presentation/controller/home_controller.dart
@@ -1,9 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:shamiri/di/locator.dart';
+import 'package:shamiri/features/feature_home/domain/use_cases/home_use_cases.dart';
+
+import '../../../feature_merchant_store/domain/model/product_model.dart';
 
 class HomeController extends GetxController {
+  final useCases = locator.get<HomeUseCases>();
+
   /// Active Bottom Bar Index
   final activeBottomBarIndex = 0.obs;
 
-  void setActiveBottomBarIndex(int index) =>
-      activeBottomBarIndex.value = index;
+  void setActiveBottomBarIndex(int index) => activeBottomBarIndex.value = index;
+
+  /// All Products
+  Stream<QuerySnapshot> getAllProducts() => useCases.getAllProducts();
 }
