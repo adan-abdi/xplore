d47bbd8bf2ac6eb52b9139605ff0631ff6ba6de7
KenStarry <starrycodes@gmail.com>
2023-08-01 11:54:40 +0300

    Added Home Use cases to get all products

---
+++ lib/features/feature_home/domain/use_cases/get_all_products.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shamiri/di/locator.dart';
+import 'package:shamiri/features/feature_home/domain/repository/home_repository.dart';
+
+class GetAllProducts {
+  final repository = locator.get<HomeRepository>();
+
+  Stream<QuerySnapshot> call() => repository.getAllProducts();
+}
