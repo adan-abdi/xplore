b88dbdd6b887c55d571a00be4664eb313d252b16
KenStarry <starrycodes@gmail.com>
2023-08-05 13:33:36 +0300

    Added Image Picker content

--- lib/features/feature_onboarding/presentation/components/image_picker_bottom_sheet.dart
+++ lib/features/feature_onboarding/presentation/components/image_picker_bottom_sheet.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shamiri/features/feature_onboarding/presentation/components/image_picker_item.dart';
 
 class ImagePickerBottomSheet extends StatelessWidget {
   const ImagePickerBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-
+        Text("Pick profile image"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ImagePickerItem(
+                iconData: Icons.camera_alt_rounded, title: "Camera", onTap: () {}),
+            ImagePickerItem(
+                iconData: Icons.image_rounded, title: "Gallery", onTap: () {}),
+            ImagePickerItem(
+                iconData: Icons.delete_forever_rounded,
+                title: "Remove Profile",
+                onTap: () {}),
+          ],
+        ),
       ],
     );
   }
