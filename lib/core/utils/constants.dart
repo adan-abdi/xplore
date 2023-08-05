44e0e55a9e3d7f76be66439864329f0237d795f2
KenStarry <starrycodes@gmail.com>
2023-08-06 00:53:34 +0300

    Added Product Categories

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,5 +1,7 @@
-class Constants {
+import 'package:flutter/material.dart';
+import 'package:shamiri/core/presentation/model/product_category.dart';
 
+class Constants {
   /// Firebase
   static const USER_COLLECTION = 'users';
   static const PRODUCTS_COLLECTION = 'products';
@@ -8,5 +10,17 @@ class Constants {
   static const USER_PREFS_BOX = 'user_prefs';
 
   /// Product Categories
-  static const productCategories = [];
+  static List<ProductCategory> productCategories = <ProductCategory>[
+    ProductCategory(
+        categoryName: 'Entertainment', categoryIcon: Icons.tv_rounded),
+    ProductCategory(
+        categoryName: 'Food & Drinks', categoryIcon: Icons.fastfood_rounded),
+    ProductCategory(
+        categoryName: 'Hardware', categoryIcon: Icons.hardware_rounded),
+    ProductCategory(
+        categoryName: 'Health & Beauty',
+        categoryIcon: Icons.health_and_safety_rounded),
+    ProductCategory(
+        categoryName: 'Computing', categoryIcon: Icons.computer_rounded),
+  ];
 }
