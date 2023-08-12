395e941e4a4b71ae61e9a534ab1efb117b563321
KenStarry <starrycodes@gmail.com>
2023-08-12 11:40:12 +0300

    Modified the merchant product bottomsheet

--- lib/features/feature_merchant_store/presentation/components/product_actions_bottom_sheet.dart
+++ lib/features/feature_merchant_store/presentation/components/product_actions_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamiri/core/utils/constants.dart';
 import 'package:shamiri/core/utils/extensions/string_extensions.dart';
 import 'package:shamiri/features/feature_merchant_store/domain/model/product_model.dart';
 
@@ -6,7 +7,6 @@ import '../../../../application/core/themes/colors.dart';
 import '../../../../domain/value_objects/app_spaces.dart';
 
 class ProductActionsBottomSheet extends StatelessWidget {
-
   final ProductModel product;
 
   const ProductActionsBottomSheet({super.key, required this.product});
@@ -16,7 +16,7 @@ class ProductActionsBottomSheet extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: MediaQuery.of(context).size.height * 0.5,
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
           //  product details header
@@ -34,17 +34,17 @@ class ProductActionsBottomSheet extends StatelessWidget {
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(24),
                     child: product.productImageUrl != null &&
-                        product.productImageUrl!.isNotEmpty
+                            product.productImageUrl!.isNotEmpty
                         ? Image.network(
-                      product.productImageUrl!,
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.cover,
-                    )
+                            product.productImageUrl!,
+                            width: double.infinity,
+                            height: double.infinity,
+                            fit: BoxFit.cover,
+                          )
                         : Icon(
-                      Icons.add_shopping_cart_rounded,
-                      color: XploreColors.white,
-                    ),
+                            Icons.add_shopping_cart_rounded,
+                            color: XploreColors.white,
+                          ),
                   ),
                 ),
 
@@ -54,20 +54,95 @@ class ProductActionsBottomSheet extends StatelessWidget {
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        product.productName!,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),
-                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
+                            product.productName!,
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          PopupMenuButton(
+                            itemBuilder: (context) => [
+                              //  delete product
+                              PopupMenuItem(
+                                onTap: () {},
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.delete_forever_rounded,
+                                      size: 16,
+                                      color: Theme.of(context).iconTheme.color,
+                                    ),
+                                    const SizedBox(
+                                      width: 8,
+                                    ),
+                                    Text(
+                                      "Delete Product",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                            color: XploreColors.white,
+                            icon: Icon(
+                              Icons.more_vert_rounded,
+                              color: XploreColors.deepBlue,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.monetization_on_rounded,
+                            color: XploreColors.xploreOrange,
+                          ),
+                          hSize10SizedBox,
+                          Text(
                               'Ksh. ${product.productSellingPrice!.toString().addCommas}',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //  category
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 4),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: XploreColors.xploreOrange),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  product.productCategoryId!.isNotEmpty
+                                      ? Constants.productCategoriesFiltered
+                                          .firstWhere((category) =>
+                                              category.categoryName ==
+                                              product.productCategoryId!)
+                                          .categoryIcon
+                                      : Icons.category_rounded,
+                                  color: XploreColors.white,
+                                  size: 16,
+                                ),
+                                hSize5SizedBox,
+                                Text(
+                                  '${product.productCategoryId!.isEmpty ? "No category" : product.productCategoryId!}',
+                                  style: TextStyle(color: XploreColors.white),
+                                ),
+                              ],
+                            ),
+                          ),
                           Text(
                               '${product.productStockCount!.toString().addCommas} in stock'),
                         ],
