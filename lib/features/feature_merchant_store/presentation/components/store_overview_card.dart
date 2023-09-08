7582ee63fd13e1a16cf9f18775d2db4f6b4fb36a
KenStarry <starrycodes@gmail.com>
2023-09-08 11:15:48 +0300

    Redesigned Store Overview Card

--- lib/features/feature_merchant_store/presentation/components/store_overview_card.dart
+++ lib/features/feature_merchant_store/presentation/components/store_overview_card.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:shamiri/application/core/themes/colors.dart';
 import 'package:shamiri/core/domain/model/user_model.dart';
 import 'package:shamiri/core/presentation/components/profile_pic.dart';
-import 'package:shamiri/core/presentation/controller/auth_controller.dart';
 import 'package:get/get.dart';
 import 'package:shamiri/core/utils/extensions/string_extensions.dart';
 import 'package:shamiri/domain/value_objects/app_spaces.dart';
 import 'package:shamiri/features/feature_merchant_store/presentation/controller/merchant_controller.dart';
-import 'package:get/get.dart';
 
 class StoreOverviewCard extends StatefulWidget {
   final UserModel store;
@@ -53,11 +51,27 @@ class _StoreOverviewCardState extends State<StoreOverviewCard> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Hello",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: XploreColors.white)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Hello",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: XploreColors.white)),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: XploreColors.xploreOrange),
+                              child: Text(
+                                "Aggregate",
+                                style: TextStyle(color: XploreColors.white),
+                              ),
+                            )
+                          ],
+                        ),
                         Text(widget.store.userName!.trimUserName,
                             style: TextStyle(
                                 fontWeight: FontWeight.bold,
@@ -73,48 +87,20 @@ class _StoreOverviewCardState extends State<StoreOverviewCard> {
 
               //  total stock count
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text("Total Stock",
-                              style: TextStyle(
-                                  fontSize: 16, color: XploreColors.white)),
-                          hSize10SizedBox,
-                          Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 12, vertical: 4),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                color: XploreColors.xploreOrange),
-                            child: Text(
-                              "Aggregate",
-                              style: TextStyle(color: XploreColors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      vSize20SizedBox,
-                      Obx(
-                        () => Text.rich(TextSpan(children: [
-                          TextSpan(
-                              text: "Ksh. ",
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: XploreColors.xploreOrange)),
-                          TextSpan(
-                              text: _merchantController.calculateTotalStock
-                                  .toString().addCommas,
-                              style: TextStyle(
-                                  fontSize: 26, color: XploreColors.white, fontWeight: FontWeight.bold)),
-                        ])),
-                      ),
-                    ],
+                  Text("Total Stock:",
+                      style:
+                          TextStyle(fontSize: 16, color: XploreColors.white)),
+                  hSize10SizedBox,
+                  Obx(
+                    () => Text(
+                        "Ksh. ${_merchantController.calculateTotalStock.toString().addCommas}",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: XploreColors.white,
+                            fontWeight: FontWeight.bold)),
                   ),
                 ],
               )
